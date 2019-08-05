357bcb6da81d01e69ce1fc5cff3ae3964bc8a8af
Gondai Mgano <gondaimgano@gmail.com>
2019-08-05 15:55:31 +0200

    added key to TravelItem class

--- app/src/main/java/com/gondai/alcchallengeapp/challenge2/TravelItem.kt
+++ app/src/main/java/com/gondai/alcchallengeapp/challenge2/TravelItem.kt
@@ -1,6 +1,7 @@
 package com.gondai.alcchallengeapp.challenge2
 
-data class TravelItem (var place:String?,
+data class TravelItem (  var key:String?,
+                         var place:String?,
                        var description:String?,
                        var amount:String?,
                        var imageURL:String?)
