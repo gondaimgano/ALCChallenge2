a98962a4d20560708dbbc81fdb5db7fa844d089f
Gondai Mgano <gondaimgano@gmail.com>
2019-08-05 15:31:13 +0200

    ImagePicker

--- app/src/main/java/com/gondai/alcchallengeapp/challenge2/DealActivity.kt
+++ app/src/main/java/com/gondai/alcchallengeapp/challenge2/DealActivity.kt
@@ -1,10 +1,15 @@
 package com.gondai.alcchallengeapp.challenge2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.esafirm.imagepicker.features.ImagePicker
+import com.esafirm.imagepicker.model.Image
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class DealActivity : AppCompatActivity() {
 
@@ -15,7 +20,7 @@ class DealActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when(item!!.itemId){
-            R.id.mnu_savedeal->Toast.makeText(this,"Save",Toast.LENGTH_SHORT).show()
+            R.id.mnu_savedeal->ImagePicker.create(this).start()
         }
 
         return super.onOptionsItemSelected(item)
@@ -25,5 +30,15 @@ class DealActivity : AppCompatActivity() {
         invalidateOptionsMenu()
         menuInflater.inflate(R.menu.savedeal,menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (ImagePicker.shouldHandle(requestCode, resultCode, data)) {
+
+            // or get a single image only
+            val image = ImagePicker.getFirstImageOrNull(data)
+            Glide.with(this).load(image).into(previewImage)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
